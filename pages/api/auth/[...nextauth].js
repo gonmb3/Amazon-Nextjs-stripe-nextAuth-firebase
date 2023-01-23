f86f41266f35450be3ab073ac5c51daefebf7247
Gonzalo Mos <gon.mb3@gmail.com>
2023-01-23 18:22:25 +0100

    env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ export const authOptions ={
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
-            secret:process.env.NEXTAUTH_URL,
+            secret:process.env.SECRET,
         }),
         
     ],
